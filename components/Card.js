13264f322024f4af60d7f6c6cc24984631432d4c
Veivesh Krishnakumar <veivesh95@gmail.com>
2019-01-16 20:10:06 +0530

    Card view

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+
+export default class Card extends React.Component {
+	render() {
+		return <View />;
+	}
+}
